c207849c90db45eb0cc511494f470aa5024dc9b8
nitzanw <nitzanw@tikalk.com>
2020-04-05 16:27:44 +0300

    add type to provider

--- movie_app/lib/application.dart
+++ movie_app/lib/application.dart
@@ -3,10 +3,12 @@ import 'package:movieapp/pages/home_page.dart';
 import 'package:movieapp/services/dio_movie_db_api.dart';
 import 'package:provider/provider.dart';
 
+import 'services/movie_db_api.dart';
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider(
+    return Provider<MovieDbApi>(
       create: (_) => DioMovieDbApi(),
       child: MaterialApp(
         title: "Movie app",
